cc0f7b16afa83f7b288c9cc6eaa9b8ef07e858eb
JeffreyYang <jeffreyhasmail@gmail.com>
2018-02-10 14:30:12 -0800

    asdf

--- charType.c
+++ charType.c
@@ -3,8 +3,10 @@
 #include<stdlib.h>
 #include<string.h>
 int main(int argc, char* argv[]){
-    //FILE* in;
-    //FILE* out;
+    FILE* in;
+    in = fopen(argv[1], "r");
+    FILE* out;
+    out = fopen(argv[2], "w");
     char* line;
     line  = malloc(6969);
     char* letters;
@@ -17,30 +19,54 @@ int main(int argc, char* argv[]){
     spaces = malloc(6969);
     char ch;
     int i,j, count;
-    if(strlen(letters) != 1){
-	   fprintf(out, "%d alphabetic characters: %s \n", strlen(letters), letters);
-    }
-    else{
-	   fprintf(out, "1 alphabetic character: %s \n", letters);
-    }
-    if(strlen(numbers) != 1){
-	    fprintf(out, "%d numeric characters: %s \n", strlen(numbers), numbers);
-    }
-    else{
-	    fprintf(out, "1 numeric character: %s \n", numbers);
-    }
-    if(strlen(punctuation) != 1){
-	    fprintf(out, "%d punctuation characters: %s \n", strlen(punctuation), punctuation);
-    }
-    else{
-	    fprintf(out, "1 punctuation character: %s \n", punctuation);
-    }
-    if(strlen(spaces) != 1){
-	    fprintf(out, "%d punctuation characters: %s \n", strlen(spaces), spaces);
-    }
-    else{
-	    fprintf(out, "1 punctuation character: %s \n", spaces);
-    }
+    int linenumber = 1;
+    // check command line for correct number of arguments
+   if(argc != 3){
+      printf("Usage: %s input-file output-file\n", argv[0]);
+      exit(EXIT_FAILURE);
+   }
+
+   // open input file for reading
+   if((in=fopen(argv[1], "r")) == NULL){
+      printf("Unable to read from %s\n", argv[1]);
+      exit(EXIT_FAILURE);
+   }
+
+   // open output file for writing   
+   if((out=fopen(argv[2], "w")) == NULL){
+      printf("Unable to write to file %s\n", argv[2]);
+      exit(EXIT_FAILURE);
+   }
+
+    while(fgets(line, 696, in) != NULL){
+    	fprintf(out, "line %d contains:\n", linenumber);
+    	extract_chars(line, letters, numbers, punctuation,spaces);
+	    if(strlen(letters) != 1){
+		   fprintf(out, "%d alphabetic characters: %s \n", strlen(letters), letters);
+	    }
+	    else{
+		   fprintf(out, "1 alphabetic character: %s \n", letters);
+	    }
+	    if(strlen(numbers) != 1){
+		    fprintf(out, "%d numeric characters: %s \n", strlen(numbers), numbers);
+	    }
+	    else{
+		    fprintf(out, "1 numeric character: %s \n", numbers);
+	    }
+	    if(strlen(punctuation) != 1){
+		    fprintf(out, "%d punctuation characters: %s \n", strlen(punctuation), punctuation);
+	    }
+	    else{
+		    fprintf(out, "1 punctuation character: %s \n", punctuation);
+	    }
+	    if(strlen(spaces) != 1){
+		    fprintf(out, "%d punctuation characters: %s \n", strlen(spaces), spaces);
+	    }
+	    else{
+		    fprintf(out, "1 punctuation character: %s \n", spaces);
+	    }
+	    linenumber++;
+	}
 				
     return EXIT_SUCCESS;
 }
